c9c6040595be7b7b13841dbe45c3a413f95280d2
shunta071 <2095071@gmail.com>
2018-09-29 16:31:55 +0000

    スマホ対応

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,11 +7,20 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
   end
 
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Comment.find(params[:id])
+    @post.update params.require(:title,:text ).permit(:image_name) # POINT
+    redirect_to @comment
+  end
+
   def new
   end
 
   def create
-
     @post = Post.new(
       title: params[:title], 
       text: params[:text]
@@ -28,6 +37,23 @@ class PostsController < ApplicationController
     @post.save
 
     redirect_to("/posts/index")
+
+    # @post = Post.new(
+    #   title: params[:title], 
+    #   text: params[:text]
+      
+    #   # TODO
+    #   # ファイル名を投稿IDと揃うようにする
+    
+    #   )
+    
+    # @post.img_name = "#{@post.title}.jpg"
+    # image = params[:img_name]
+    # File.binwrite("public/post_images/#{@post.img_name}", image.read)
+
+    # @post.save
+
+    # redirect_to("/posts/index")
     
   end
 
